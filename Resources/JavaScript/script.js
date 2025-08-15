3102af2fc512bb9c270c86aa0eb81523dc94e970
Devin <devinsloan2005@gmail.com>
2025-08-14 21:18:25 -0400

    Homepage Development

---
+++ Resources/JavaScript/script.js
@@ -0,0 +1,11 @@
+// Show Projects
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+// Hide Projects
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
